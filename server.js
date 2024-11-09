87e801932094e0c602dcfdae2e3b48c81b810483
Monish Jaiswal <monishj8956@gmail.com>
2024-11-09 20:44:08 +0530

    improved spacing in server.js

--- server.js
+++ server.js
@@ -2,15 +2,11 @@ const express = require('express');
 require('dotenv').config();
 const weatherRoutes = require('./routes/weatherRoutes');
 
-
-
-
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 app.use(weatherRoutes);
-
 
 app.listen(PORT, () => {
     console.log(`Server listening on port: ${PORT}....`);
